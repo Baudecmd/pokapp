b300570893ba9b93150d151c614229ad6ed068e5
Conrad BAUDELET <conrad.baudelet@outlook.com>
2021-05-30 16:56:33 +0200

    Refacto

---
+++ lib/SplashScreenPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:pokapp/HomePage.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+class SplashScreenPage extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => new _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreenPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreenView(
+      navigateRoute: HomePage(),
+      duration: 3000,
+      imageSize: 130,
+      imageSrc: "assets/images/logo.png",
+      backgroundColor: Colors.white,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:splash_screen_view/SplashScreenView.dart';
+import 'package:pokapp/SplashScreenPage.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,42 +15,5 @@ class MyApp extends StatelessWidget {
       ),
       home: SplashScreenPage(),
     );
-  }
-}
-
-class SplashScreenPage extends StatefulWidget {
-  @override
-  _SplashScreenState createState() => new _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreenPage> {
-  @override
-  Widget build(BuildContext context) {
-    return new SplashScreenView(
-      navigateRoute: HomePage(),
-      duration: 3000,
-      imageSize: 130,
-      imageSrc: "assets/images/logo.png",
-      backgroundColor: Colors.white,
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    return new _homePage();
-  }
-}
-
-class _homePage extends State<HomePage> {
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Page accueil"),
-        ),
-        body: Row(children: [Text("Page accueil de Pokapp")]));
   }
 }
